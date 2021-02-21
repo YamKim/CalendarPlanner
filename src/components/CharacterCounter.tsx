28bc06fc0517e0da5ee8415b9cbdc587aea639cd
Kim Tae Hoon <eszqsc112@gmail.com>
2021-02-22 07:27:53 +0900

    [dividing-recoil] add new folder and lf settings

--- src/components/CharacterCounter.tsx
+++ src/components/CharacterCounter.tsx
@@ -2,25 +2,15 @@ import React from "react";
 
 import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
 
+import {} from '../state/character'
+
 function CharacterCount() {
   const count = useRecoilValue(charCountState);
 
   return <>Character Count: {count}</>;
 }
 
-const charCountState = selector({
-  key: "charCountState", // unique ID (with respect to other atoms/selectors)
-  get: ({ get }) => {
-    const text = get(textState);
 
-    return text.length;
-  },
-});
-
-export const textState = atom({
-  key: "textState", // unique ID (with respect to other atoms/selectors)
-  default: "", // default value (aka initial value)
-});
 
 function TextInput() {
   const [text, setText] = useRecoilState(textState);
